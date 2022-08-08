f2a1a9a2e04eb6c8ba6d2f4b3aea33344b3e33f5
emanuel acosta <wherehog21@gmail.com>
2022-08-08 01:10:27 -0300

    initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div
+      name="Contacto"
+      className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white"
+    >
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Contacto
+          </p>
+        </div>
+
+        <div className=" flex justify-center items-center">
+          <form
+            action="https://getform.io/f/adda3ce9-6740-4869-92d7-57f385eee062"
+            method="POST"
+            className=" flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Escribe tu nombre"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="text"
+              name="email"
+              placeholder="Escribe tu email"
+              className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              placeholder="Escribe un mensaje"
+              rows="10"
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+             Enviar
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import contarClicks from "../assets/portfolio/contarClicks.png";
+import climaApp from "../assets/portfolio/climaApp.png";
+import toDoList from "../assets/portfolio/toDoList.png";
+import calculadora from "../assets/portfolio/calculadora.png";
+import relojjs from "../assets/portfolio/relojs-js.png";
+import pokedex from "../assets/portfolio/pokedexApp.png";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: contarClicks,
+      nombre:"Contador de Clicks",
+      link:"https://contarclicks.netlify.app/",
+      link2:"https://github.com/emaaa182/contador-clicks-vanilla-js"
+     
+    },
+    {
+      id: 2,
+      src: calculadora,
+      nombre:"Calculadora",
+      link:"https://calculadora-basica-demo.netlify.app/",
+      link2:"https://github.com/emaaa182/basic-calculator-js"
+    },
+    {
+      id: 3,
+      src: toDoList,
+      nombre:"Lista de Tareas",
+      link:"https://lista-de-tareas-demo.netlify.app/",
+      link2:"https://github.com/emaaa182/to-do-list"
+    },
+    {
+      id: 4,
+      src: relojjs,
+      nombre:"Reloj Basico",
+      link:"https://reloj-js-demo.netlify.app/",
+      link2:"https://github.com/emaaa182/reloj-JS"
+    },
+    {
+      id: 5,
+      src: climaApp,
+      nombre:"ClimaApi",
+      link:"https://clima-app-demo.netlify.app/",
+      link2:"https://github.com/emaaa182/weather-app-js"
+    },
+    {
+      id: 6,
+      src: pokedex,
+      nombre:"PokedexApi",
+      link:"https://pokedex-api-demo.netlify.app/",
+      link2:"https://github.com/emaaa182/pokedex"
+    },
+  ];
+
+  return (
+    <div
+      name="Portfolio"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src,nombre, link,link2 }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <h3>{nombre}</h3>
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+              <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <a href={link} target="blank">Demo</a>
+                </button>
+                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  <a href={link2} target="blank">Code</a>
+                </button>
+            
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
